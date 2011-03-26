a0663fc4ced9cf8a0214401b1ece6fd7e84ea690
bart <elbarto@gmail.com>
2011-03-26 10:16:31 -0400

    added some explanation comments to main

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -21,6 +21,24 @@ Player *g_player;
 
 sf::Font fontUI;
 
+/* TODO:
+ * 
+ * Okay, we need to clean this stuff up.  The update function should be empty
+ * except for the loop that checks all actors and runs their update functions.
+ * Player should be an actor, and should update when the other actors do.
+ * Input and collision handling for the player need to go in the player's 
+ * update() function.
+ * 
+ * The collide() function is intended to be an event that is called from an
+ * actor's update() function when it collides with another object.  Each frame,
+ * Actors should iterate through the list and see what's colliding with them,
+ * then run their own collide() function, passing the other actor as the argument.
+ * Enemies and enemy bullets are exempt from this, unless we want them colliding
+ * with each other (we can handle the important collisions from the Player and
+ * PlayerBullet classes.
+ * 
+ * */
+
 void update(Player& player, float dt) {
 	for (list<Actor*>::iterator it = actors.begin(); it != actors.end(); ++it)
 		(*it)->update(dt);
