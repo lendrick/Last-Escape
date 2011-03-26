a95893e63de309575da0a954a39f08b4ad6e845c
bart <elbarto@gmail.com>
2011-03-25 22:02:06 -0400

    actor class

--- src/game/Actor.cpp
+++ src/game/Actor.cpp
@@ -1,1 +1,73 @@
- 
+#include "Actor.h"
+
+Actor::Actor() {
+	setPos(0, 0);
+	destroyed = false;
+}
+
+Actor::~Actor() {
+}
+
+void Actor::setPos(int px, int py) {
+	px = x;
+	py = y;
+}
+
+void Actor::move(int mx, int my) {
+	x += mx;
+	y += my;
+}
+
+void Actor::getPos(int &px, int &py) {
+	px = x;
+	py = y;
+}
+
+void Actor::setSize(int w, int h) {
+	height = h;
+	width = w;
+}
+
+void Actor::getSize(int &w, int &h) {
+	h = height;
+	w = width;
+}
+
+void Actor::setOrigin(int ox, int oy) {
+	xOrigin = ox;
+	yOrigin = oy;
+}
+
+void Actor::getOrigin(int &ox, int &oy) {
+	ox = xOrigin;
+	oy = yOrigin;
+}
+
+void Actor::getBoundingBox(int &x1, int &y1, int &x2, int &y2) {
+	x1 = x - xOrigin;
+	y1 = y - yOrigin;
+	x2 = x1 + width;
+	y2 = y1 + heightl
+}
+
+bool Actor::isColliding(Actor * otherActor) {
+	int x1, y1, x2, y2;
+	int ox1, oy1, ox2, oy2;
+	getBoundingBox(x1, y1, x2, y2);
+	otherActor->getBoundingBox(ox1, oy1, ox2, oy2);
+	
+	if( ( (x1 < ox1 && ox1 < x2) || (x1 < ox2 && ox2 < x2) ) &&
+		( (y1 < oy1 && oy1 < y2) || (y1 < oy2 && oy2 < y2) ) ) {
+		return true;
+	}
+	
+	return false;
+}
+
+void Actor::destroy() {
+	destroyed = true;
+}
+
+bool Actor::isDestroyed() {
+	return destroyed;
+}
--- src/game/Actor.h
+++ src/game/Actor.h
@@ -1,1 +1,33 @@
- 
+#pragma once
+#include <globals.h>
+
+class Actor {
+public:
+	Actor();
+	~Actor();
+	void setPos(float px, float py);
+	void move(float mx, float my);
+	void getPos(float &px, float &py);
+	
+	void setSize(int w, int h);
+	void getSize(int &w, int &h);
+	
+	void setOrigin(int ox, int oy);
+	void getOrigin(int &ox, int &oy);
+	
+	void getBoundingBox(int &x1, int &y1, int &x2, int &y2);
+	
+	bool isColliding(Actor * otherActor);
+	
+	virtual void update() = 0;
+	virtual void draw() = 0;
+	
+	void destroy();
+	bool isDestroyed();
+	
+private:
+	int xOrigin, yOrigin;
+	int height, width;
+	float x, y;
+	bool destroyed;
+};
