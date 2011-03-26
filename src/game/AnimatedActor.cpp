d493cecd46855a2d08c0a00ec52be45386ac64f1
Nehmulos <Nehmulos@inbox.com>
2011-03-26 13:46:28 +0100

    Implemented AnimatedActor;

--- src/game/AnimatedActor.cpp
+++ src/game/AnimatedActor.cpp
@@ -1,1 +1,38 @@
- 
+ #include "AnimatedActor.h"
+
+AnimatedActor::AnimatedActor(sf::Image& image)
+{
+	//testing values
+//	this->setOrigin(64.0f, 400.0f);
+//	this->setSize(24, 48);
+	this->sprite.SetImage(image);
+	this->sprite.SetX(64.f);
+	this->sprite.SetY(400.f);
+	this->sprite.Resize(24, 48);
+
+	this->currentAnimation = new Animation(this->sprite);
+	this->currentAnimation->toDefaultXeonAnimation(); //testing 
+
+//	this->animationQueue.push(new Animation(&this->sprite));
+//	this->animationQueue.front()->toDefaultAnimation();
+}
+
+AnimatedActor::~AnimatedActor()
+{
+}
+
+void AnimatedActor::update()
+{
+	this->currentAnimation->update();
+}
+
+// not tested, yet. Writen in nano without autocorrection
+void AnimatedActor::draw()
+{
+	App->Draw(this->sprite);
+}
+
+// implemented for testing
+void AnimatedActor::collide(Actor* other)
+{
+}
--- src/game/AnimatedActor.h
+++ src/game/AnimatedActor.h
@@ -1,4 +1,23 @@
 #pragma once
-
+#include "globals.h"
+#include "Animation.h"
+#include "Actor.h"
 // This class is for all actors with animations (or even still actors).  It inherits Actor.
 
+class AnimatedActor : public Actor
+{
+public:
+	AnimatedActor(sf::Image&);
+	virtual ~AnimatedActor();
+	void collide(Actor*); 	///< implemented empty for testing;
+	void update();  		///< update Animation
+	void draw();    		///< draw Current AnimationFrame
+
+private:
+	Animation *currentAnimation; ///< TODO use a queue
+	std::map<std::string, Animation*> animations; ///< stores all available Animations for this Actor
+	
+	//Drawing resources may move into a simper Actor subclass that implements simple drawing
+	sf::Image image;
+	sf::Sprite sprite;
+};
