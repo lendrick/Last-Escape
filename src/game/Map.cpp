7772b69f7a1f0455be592a65958e10935a5eed05
Lucas Beyer <pompei2@gmail.com>
2011-03-27 22:35:23 +0200

    Render the fringe behind the hero, but above the background.

--- src/game/Map.cpp
+++ src/game/Map.cpp
@@ -462,6 +462,17 @@ void Map::renderBackground() {
 		}
 	}
 
+	// render fringe
+	for (int i=0; i<VIEW_TILES_X; i++) {
+		if (cam_tile_x + i < 0 || cam_tile_x + i >= MAP_TILES_X) continue;
+		for (int j=0; j<VIEW_TILES_Y; j++) {
+			if (cam_tile_y + j < 0 || cam_tile_y + j >= MAP_TILES_Y) continue;
+			if (!fringe[cam_tile_x + i][cam_tile_y + j]) continue;
+			tile_sprites[i][j].SetSubRect(tile_rects[fringe[cam_tile_x + i][cam_tile_y + j]]);
+			App->Draw(tile_sprites[i][j]);
+		}
+	}
+
 }
 
 void Map::actorDestroyed(Actor * actor) {
@@ -485,17 +496,6 @@ void Map::renderForeground() {
 	for (int i=0; i<VIEW_TILES_X; i++) {
 		for (int j=0; j<VIEW_TILES_Y; j++) {
 			tile_sprites[i][j].SetPosition(i*32 - cam_off_x + 0.5f, j*32 - cam_off_y + 0.5f);
-		}
-	}
-
-	// render fringe
-	for (int i=0; i<VIEW_TILES_X; i++) {
-		if (cam_tile_x + i < 0 || cam_tile_x + i >= MAP_TILES_X) continue;
-		for (int j=0; j<VIEW_TILES_Y; j++) {
-			if (cam_tile_y + j < 0 || cam_tile_y + j >= MAP_TILES_Y) continue;
-			if (!fringe[cam_tile_x + i][cam_tile_y + j]) continue;
-			tile_sprites[i][j].SetSubRect(tile_rects[fringe[cam_tile_x + i][cam_tile_y + j]]);
-			App->Draw(tile_sprites[i][j]);
 		}
 	}
 
