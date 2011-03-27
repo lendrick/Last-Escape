b06d7d9b2c848ffe8a7e24aa104c5240da53c8be
Lucas Beyer <pompei2@gmail.com>
2011-03-27 19:01:39 +0200

    Fixed map and player loading so that on each new level or game over, all enemies are present again, but not present twice!

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -22,7 +22,7 @@
 list<Actor *> actors;
 Map * game_map;
 sf::RenderWindow *App;
-Player *g_player;
+Player *g_player = 0;
 Input input;
 bool godMode = false;
 
@@ -94,9 +94,6 @@ int main(int argc, char** argv)
 
 	// Create game objects
 	game_map = new Map(mapName);
-	Player p1;
-	
-	g_player = &p1;
 	game_map->setCameraFollow(g_player);
 
 
@@ -128,7 +125,7 @@ int main(int argc, char** argv)
 		App->Clear();
 		
 		if(!paused) 
-			update(p1, frameTime);
+			update(*g_player, frameTime);
 		
 		cleanup();
 
@@ -137,12 +134,13 @@ int main(int argc, char** argv)
 		renderActors();
 		game_map->renderForeground();
 
-		ui_render(p1);
+		ui_render(*g_player);
 
 		// Finally, display the rendered frame on screen
 		App->Display();
 	}
 
+	delete g_player;
 	delete game_map;
 	delete App;
 
