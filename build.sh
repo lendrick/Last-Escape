405a0cc4c07244159cae4e22c5fb19d28ccc0b63
Nehmulos <Nehmulos@inbox.com>
2011-10-08 00:16:21 +0200

    fixed cd; avoid false error output; echo an error, if the build directory can not be created;

--- build.sh
+++ build.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
-cd build || mkdir build && cd build
-cmake ../src
-make
+
+#if build directory doesn't exists
+if [ ! -d "build" ]; then
+    mkdir build
+    echo "created build directory"
+fi
+
+#sucessed to create build directory
+if [ -d "build" ]; then   
+    cd build
+    cmake ../src
+    make
+else
+    echo "Failed to create build directory"
+fi
