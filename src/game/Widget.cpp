927e9e2389ab4bc1d8f630e8638647e4b626d5ea
Nehmulos <Nehmulos@inbox.com>
2011-10-27 03:32:43 +0200

    GPLed;

--- src/game/Widget.cpp
+++ src/game/Widget.cpp
@@ -1,8 +1,18 @@
 /*
- * Widget.cpp
+ *  This file is part of Last Escape.
  *
- *  Created on: 25.10.2011
- *      Author: nehmulos
+ *  Last Escape is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Last Escape is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Last Escape.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "Widget.h"
